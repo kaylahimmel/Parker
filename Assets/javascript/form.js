bad48e35750fd948adec924eaa47d5cf0534071f
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2019-05-05 17:58:12 -0400

    created form.js file with psuedocode

---
+++ Assets/javascript/form.js
@@ -0,0 +1,23 @@
+{/* <form>
+  <div id="input-container">
+    <div class=stateList>
+      <form id="simpleStateForm">
+        <p id="selectStateText">Select a US state/territory:</p>
+        <select name="stateSelect" id="stateSelection">
+          <option></option>
+        </select>
+      </form>
+    </div>
+  </div>
+</form> */}
+
+
+// make form with 
+
+
+// dropdown list of states/territories
+
+
+// submit button
+
+
